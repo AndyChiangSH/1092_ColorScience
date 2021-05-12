e45d053be78bb3ae91896c01d10d6d5ea145792c
Andy Chiang <andy10801@gmail.com>
2021-05-12 23:26:07 +0800

    hw10 init

--- 作業09/test.py
+++ 作業09/test.py
@@ -1,15 +1,4 @@
-from decimal import Decimal
+n = int(input())
 
-x = 0.1247185487026306
-
-print(str(x))
-
-d = Decimal(str(x))
-
-print(round(d, 6))  # 0.124719
-print(type(d))      # <class 'decimal.Decimal'>
-
-d = float(round(d, 6))
-
-print(d)
-print(type(d))
+for i in range(n):
+    print("Hello, world!")
